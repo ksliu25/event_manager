32e1a33eaea08bfc3922ac65faf6d9ac49b128d0
ksliu25 <ksliu25@gmail.com>
2015-02-18 21:55:17 -0600

    Commit before bonus iterations

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -10,9 +10,19 @@ end
 
 #leave this in for now since we are unfamilir with the API
 def legislators_by_zipcode(zipcode)
-	Sunlight::Congress::Legislator.by_zipcode(zipcode)
+    Sunlight::Congress::Legislator.by_zipcode(zipcode)
 end
 
+def save_thank_you_letters(id, form_letter)
+    Dir.mkdir("output") unless Dir.exists? "output"
+	
+    filename = "output/thanks_#{id}.html"
+  
+    File.open(filename, 'w') do |file|
+	file.puts form_letter
+      end
+  end
+  
 puts "EventManager initialized."
 
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
@@ -21,12 +31,10 @@ template_letter = File.read "form_letter.erb"
 erb_template = ERB.new template_letter
 
 contents.each do |row|
-  name = row[:first_name]
-
-  zipcode = clean_zipcode(row[:zipcode])
-  
-  legislators = legislators_by_zipcode(zipcode)
-  
-  form_letter = erb_template.result(binding)
-  puts form_letter
-end
+    id = row[0]
+    name = row[:first_name]
+    zipcode = clean_zipcode(row[:zipcode])
+    legislators = legislators_by_zipcode(zipcode)
+    form_letter = erb_template.result(binding)
+    save_thank_you_letters(id, form_letter)
+  end
